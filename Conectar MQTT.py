384ab60b6354a4542b4fe555d9d08e0a4c154b1a
JG-Soto <132724082+JG-Soto@users.noreply.github.com>
2024-02-27 21:09:44 -0500

    Add files via upload

--- Conectar MQTT.py
+++ Conectar MQTT.py
@@ -1,30 +1,45 @@
 import json
 import paho.mqtt.client as mqtt
-import Rendimiento1
-from Rendimiento1 import obtener_info_sistema
+import psutil
+import platform
+import datetime
+import uuid
+import time
 
-mqtt_broker_address = "mqtt-dashboard.com"
+mqtt_broker_address = "broker.hivemq.com"
 mqtt_port = 1883
 mqtt_topic = "Taller MQTT"
 
-informacion = obtener_info_sistema()
+def obtener_info_sistema():
+    info_sistema = {}
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Conectado con el código de resultado: {rc}")
-    client.subscribe(mqtt_topic)
+    info_sistema['temperatura'] = obtener_temperatura()
+    info_sistema['rendimiento_cpu'] = psutil.cpu_percent(interval=1)
+    info_sistema['rendimiento_memoria'] = psutil.virtual_memory().percent
+    info_sistema['rendimiento_red'] = psutil.net_io_counters().bytes_sent
 
-def on_publish(client, userdata, mid):
-    print(f"Mensaje publicado con éxito (ID: {mid})")
+    return info_sistema
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_publish = on_publish
+def obtener_temperatura():
+    try:
+        temperatures = psutil.sensors_temperatures()
+        if "coretemp" in temperatures:
+            return temperatures["coretemp"][0].current
+        elif "cpu-thermal" in temperatures:
+            return temperatures["cpu-thermal"][0].current
+        else:
+            return "Información no disponible"
+    except Exception as e:
+        return "Información no disponible"
 
-client.connect(mqtt_broker_address, mqtt_port, keepalive=60)
+def publicar_info_periodicamente():
+    client = mqtt.Client()
+    client.connect(mqtt_broker_address, mqtt_port, keepalive=60)
+    
+    while True:
+        informacion = obtener_info_sistema()
+        client.publish(mqtt_topic, json.dumps(informacion))
+        time.sleep(10)  # Esperar 10 segundos antes de publicar nuevamente
 
-client.publish(mqtt_topic, json.dumps(informacion, indent=4))
-
-client.loop_start()
-client.loop_stop()
-client.disconnect()
- 
+if __name__ == "__main__":
+    publicar_info_periodicamente()
